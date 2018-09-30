099cecb85b03322c5ac5d8691d4327b44f3d5ad7
root <root@registry.doc.htyunlu.com>
2018-09-30 18:13:17 +0800

    delete mha

--- ansible/roles/mysql-master/files/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#init mysql shell
-/usr/share/mysql/bin/mysqld \
---initialize \
---user=mysql \
---datadir=/data/mysql \
---basedir=/usr/share/mysql \
---socket=/var/lib/mysql/mysql.sock
